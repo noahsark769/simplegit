f39807fcc46aa208028b7adc1dc5bea5cea3396b
Noah Gilmore <noah.w.gilmore@gmail.com>
2016-01-17 23:48:06 -0800

    Fixes and logging

--- simplegit/git.py
+++ simplegit/git.py
@@ -10,14 +10,19 @@ class Git(object):
         """Proxy the attribute request to function that executes subprocess."""
         return self._func_for_key(key)
 
-    def call(self, key, args_string, **kwargs):
+    def call(self, key, args_string=None, **kwargs):
         return self._func_for_key(key)(args_string, **kwargs)
+
+    def _log(self, message):
+        if self.logger:
+            self.logger.info(message)
 
     def _func_for_key(self, key):
         def proxy(cmd=None, **kwargs):
             args = ["git", key]
             if cmd:
                 args.extend(shlex.split(cmd))
+            self._log("Executing command: " + " ".join(args))
             process = subprocess.Popen(
                 args,
                 stdin=subprocess.PIPE,
